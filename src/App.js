c6b926d264646c6603c6169b65d87ce2b53cde1a
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-06-19 12:10:31 -0700

    Add simple Form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <form id="to-do-form" onSubmit={this.addItem}>
+          <input
+            type="text"
+            placeholder="Enter Text"
+            value={this.state.currentItem.text}
+            onChange={this.handleInput}
+          />
+          <button type="submit">Add</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
